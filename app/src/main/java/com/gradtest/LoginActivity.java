94fc4496818602c99b00c518ea70f9aa8a196ebf
구설원 <seolwon6289@naver.com>
2018-04-08 19:16:46 +0900

    20180408 1916 로그인화면 아이디찾기화면 구축

--- app/src/main/java/com/gradtest/LoginActivity.java
+++ app/src/main/java/com/gradtest/LoginActivity.java
@@ -1,8 +1,18 @@
 package com.gradtest;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
 /**
  * Created by SM-PC on 2018-04-02.
  */
 
-public class LoginActivity {
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
 }
