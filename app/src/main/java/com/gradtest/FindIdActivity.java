5cdca1754e3ebc21d92f4daa304fc36d6fe11fda
구설원 <seolwon6289@naver.com>
2018-04-08 22:18:50 +0900

    20180408 2218 메인화면 글쓰기 화면 구축

---
+++ app/src/main/java/com/gradtest/FindIdActivity.java
@@ -0,0 +1,17 @@
+package com.gradtest;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by SM-PC on 2018-04-08.
+ */
+
+public class FindIdActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_find_id);
+    }
+}
--- app/src/main/java/com/gradtest/LoginActivity.java
+++ app/src/main/java/com/gradtest/LoginActivity.java
@@ -1,18 +1,33 @@
 package com.gradtest;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * Created by SM-PC on 2018-04-02.
  */
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        Button loginbtn = (Button)findViewById(R.id.login_btn);
+        loginbtn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.login_btn:
+                Intent intent = new Intent(this, MainActivity.class);
+                startActivity(intent);
+                break;
+        }
     }
 }
