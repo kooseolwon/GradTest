fab83861c6ddf07375b78309698ce187548295d6
kooseolwon <seolwon6289@naver.com>
2018-09-14 06:20:58 +0900

    필터 조금많이 완성햇다링링

--- app/src/main/java/com/gradtest/Activity/MainActivity.java
+++ app/src/main/java/com/gradtest/Activity/MainActivity.java
@@ -1,8 +1,7 @@
 package com.gradtest.Activity;
 
-import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
@@ -35,63 +34,96 @@ public class MainActivity extends AppCompatActivity {
     RecyclerView rcv;
     LinearLayoutManager llm;
     WritingAdapter wadapter;
-    List<String> index,time;
-    ArrayList searchAreaArray;
-    String date,id,title,item,content;
-
-    //String user_name[]={}, board_title[]={};
+    List<String> index, time;
+    String date, id, title, item, content;
+    int flag = 0;
     int switch_num = 0;
     int switchn = 0;
+    ArrayList area;
+    public static Context CONTEXT;
+    StringBuilder str;
+    ArrayList data;
+
 
 
     // 밑 부분은 필터 누르고 갖고온 결과를 처리해주는 부분임.
+
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        if(resultCode == RESULT_OK){
-            searchAreaArray = new ArrayList();
-            Log.v("searchArea","들어옴");
+        System.out.println(area);
+        System.out.println("data입니다옹" + data);
+        if (resultCode == RESULT_OK) {
+            flag = 1;
+            Log.v("snow", "드러와쬬");
+            area = data.getIntegerArrayListExtra("filterIntent");
+
         }
+        System.out.println(area);
+
+
     }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    protected void onResume() {
+        super.onResume();
         setContentView(R.layout.activity_main);
+        System.out.println("snowfield"+area);
+        str = new StringBuilder();
 
-        Intent intent = getIntent();
-        switchn = getIntent().getIntExtra("switch",0);
-        title = getIntent().getStringExtra("title");
-        content = getIntent().getStringExtra("content");
+        if(area == null){
+            str.append("[]");
+        }else{
+            str.append("[");
+                for(int i = 0; i < area.size();i++){
+                    str.append(area.get(i));
+                    if(area.size()-1 != i){
+                        str.append(',');
+                    }
+                }
+            str.append("]");
 
+        }
+
+
+
+        String arrString = str.toString();
+
+        System.out.println("onResume" + arrString);
         List<String> user_name = new ArrayList<String>();
         List<String> board_title = new ArrayList<String>();
         index = new ArrayList<String>();
         time = new ArrayList<String>();
-
-
-        Button btn_w = (Button)findViewById(R.id.writing_btn);
-                btn_w.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View view){
+        rcv = (RecyclerView) findViewById(R.id.recycler);
+        llm = new LinearLayoutManager(this);
+        rcv.setHasFixedSize(true);
+        rcv.setLayoutManager(llm);
+        Button btn_w = (Button) findViewById(R.id.writing_btn);
+        btn_w.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
                 Intent intent = getIntent();
-                int uidx = intent.getIntExtra("uidx",0);
+                int uidx = intent.getIntExtra("uidx", 0);
 
                 Intent it_w = new Intent(MainActivity.this, WritingActivity.class);
-                it_w.putExtra("switch_num",switch_num);
-                it_w.putExtra("userindex",uidx);
+                it_w.putExtra("switch_num", switch_num);
+                it_w.putExtra("userindex", uidx);
 
                 startActivity(it_w);
-                            }
+            }
         });//글쓰기 버튼
 
-        Button btn_filter = (Button)findViewById(R.id.filter_btn);
-        btn_filter.setOnClickListener(new View.OnClickListener(){
+        // 필터 인텐트
+
+
+        Button btn_filter = (Button) findViewById(R.id.filter_btn);
+        btn_filter.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
                 Intent it_filter = new Intent(MainActivity.this, filterActivity.class);
-                startActivity(it_filter);
+                startActivityForResult(it_filter, 0);
 
             }
         });
@@ -99,89 +131,45 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-
-
-        rcv = (RecyclerView)findViewById(R.id.recycler);
-        llm = new LinearLayoutManager(this);
-        rcv.setHasFixedSize(true);
-        rcv.setLayoutManager(llm);
-
         ArrayList<ItemForm> list = new ArrayList<>();
-
-
-        Call<Board> res = Net.getInstance().getNetworkService().show_board();
+        //data = new ArrayList();
+        Call<Board> res = Net.getInstance().getNetworkService().show_board(arrString);
         res.enqueue(new Callback<Board>() {
             @Override
             public void onResponse(Call<Board> call, Response<Board> response) {
-                if(response.isSuccessful()){
+                if (response.isSuccessful()) {
                     Board board = response.body();
-                    ArrayList data = response.body().getData();
-                    Log.v("seo",data.toString());
+                    data = response.body().getData();
+                    System.out.println("seolwonkoo"+data);
+                    Log.v("seo", data.toString());
 
 
-                } else{
-                int statusCode = response.code();
-                    Log.i("MyTag","응답코드 : "+statusCode);
+                } else {
+                    int statusCode = response.code();
+                    Log.i("MyTag", "응답코드 : " + statusCode);
                 }
             }
 
             @Override
             public void onFailure(Call<Board> call, Throwable t) {
-                Log.i("MyTag", "서버 onFailure 에러내용 : " +t.getMessage());
+                Log.i("MyTag", "서버 onFailure 에러내용 : " + t.getMessage());
 
             }
         });
 
-
-
-
-/*
-
-
-
-        list.add(new ItemForm("koo",R.drawable.jjang1,"서러니최고","쿠쿠쿠"));
-        list.add(new ItemForm("seolwon6289",R.drawable.jjang2,"서러니귀엽다","너무 이뻠"));
-        list.add(new ItemForm("seolwon6289",R.drawable.jjang3,"씨스타","푸시푸시베이비 내맘을 받아줘"));
-
-
-        if(switch_num==1) {
-            Toast toast_w = Toast.makeText(this,"글 작성 완료!",Toast.LENGTH_LONG);
-            toast_w.show();
-            list.add(new ItemForm(id, R.drawable.logo_image, title,content));
-            switch_num = 0;
-            wadapter = new WritingAdapter(this, list);//앞서 만든 리스트를 어뎁터에 적용시켜 객체를 만든다.
-            rcv.setAdapter(wadapter);// 그리고 만든 겍체를 리싸이클러뷰에 적용시킨다.
-        }
-
-        else {
-            wadapter = new WritingAdapter(this, list);//앞서 만든 리스트를 어뎁터에 적용시켜 객체를 만든다.
-            rcv.setAdapter(wadapter);// 그리고 만든 겍체를 리싸이클러뷰에 적용시킨다.
-        }
-
-        if(switchn ==1)
-        {
-            list.add(new ItemForm("example2",R.drawable.logo_image,title,content));
-            switchn = 0;
-            wadapter = new WritingAdapter(this, list);//앞서 만든 리스트를 어뎁터에 적용시켜 객체를 만든다.
-            rcv.setAdapter(wadapter);// 그리고 만든 겍체를 리싸이클러뷰에 적용시킨다.
-        }
-*/
-
-
-
-
-        if(android.os.Build.VERSION.SDK_INT>9){
-            StrictMode.ThreadPolicy policy=new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        if (android.os.Build.VERSION.SDK_INT > 9) {
+            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
             StrictMode.setThreadPolicy(policy);
         }
 
-        try{
+        try {
 
-            String urlStr = "http://52.78.129.27:3000/board/show";
+            String urlStr = "http://52.78.129.27:3000/board/show?board_category="+arrString;
+            System.out.println(urlStr);
             URL url = new URL(urlStr);
 
-            InputStreamReader isr = new InputStreamReader(url.openConnection().getInputStream(),"UTF-8");
-            JSONObject object = (JSONObject)JSONValue.parseWithException(isr);
+            InputStreamReader isr = new InputStreamReader(url.openConnection().getInputStream(), "UTF-8");
+            JSONObject object = (JSONObject) JSONValue.parseWithException(isr);
 
             System.out.println("jObj : " + object.toString());
             System.out.println("message : " + object.get("message"));
@@ -189,43 +177,38 @@ public class MainActivity extends AppCompatActivity {
 
 
             String main = object.toString();
-            JSONObject obj = (JSONObject)JSONValue.parse(main);
-            JSONArray array = (JSONArray)obj.get("data");
 
-            for(int i=0; i<array.size(); i++) {
+
+            JSONObject obj = (JSONObject) JSONValue.parse(main);
+            JSONArray array = (JSONArray) obj.get("data");
+
+            for (int i = 0; i < array.size(); i++) {
                 JSONObject j = (JSONObject) array.get(i);
                 System.out.println("title " + i + "번째 : " + j.get("board_title"));
                 System.out.println("id " + i + "번째 : " + j.get("user_name"));
-                System.out.println("time " +i + "번째 : " + j.get("board_time"));
+                System.out.println("time " + i + "번째 : " + j.get("board_time"));
                 user_name.add(j.get("user_name").toString());
                 board_title.add(j.get("board_title").toString());
                 index.add(j.get("board_index").toString());
                 time.add(j.get("board_time").toString());
-
-
-
-                /*list.add(new ItemForm(user_name.get(i),R.drawable.jjang1,board_title.get(i),""));
-                wadapter = new WritingAdapter(this, list);
-                rcv.setAdapter(wadapter);*/
             }
 
-            for(int i=array.size()-1; i>0; i--){
-                list.add(new ItemForm(user_name.get(i),R.drawable.jjang1,board_title.get(i),index.get(i),time.get(i)));
+            for (int i = array.size() - 1; i > 0; i--) {
+                list.add(new ItemForm(user_name.get(i), R.drawable.jjang1, board_title.get(i), index.get(i), time.get(i)));
                 wadapter = new WritingAdapter(this, list);
                 rcv.setAdapter(wadapter);
             }
 
 
-
-        }catch(Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
 
-
-
-
-
     }
 
-
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
 }
--- app/src/main/java/com/gradtest/Net/NetworkService.java
+++ app/src/main/java/com/gradtest/Net/NetworkService.java
@@ -9,10 +9,10 @@ import okhttp3.RequestBody;
 import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.GET;
-import retrofit2.http.Header;
 import retrofit2.http.Multipart;
 import retrofit2.http.POST;
 import retrofit2.http.Part;
+import retrofit2.http.Query;
 
 /**
  * Created by sm-pc on 2018-05-30.
@@ -60,7 +60,11 @@ public interface NetworkService {
 
     //게시물보기
     @GET("board/show")
-    Call<Board> show_board();
+    Call<Board> show_board(
+            @Query("board_category") String board_category
+    );
+
+
 
 
 
